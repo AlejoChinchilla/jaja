58be5763ee905352349f2a041f1b4926ddeaafca
AlejoChinchilla <abautistachinchilla@gmail.com>
2025-02-22 16:54:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function() {
+alert('Sabia que te gustaba el pene🏳️‍🌈☠️')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function() {
+alert('Aja ya lo provaste HDP🤨📷')
+});
